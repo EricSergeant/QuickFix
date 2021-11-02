f012869ba49384bbf796b6af741ce386637d2519
Raquel Hill <raquelturing@gmail.com>
2021-11-01 22:18:35 -0600

    Begin structure for nav bar and create initial styling for navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
 
   return (
-    <h3>NavBar goes here!</h3>
+    <nav className="sidenav">
+      <p className="biography">Biography</p>
+      <p className="children">Children</p>
+      <p className="fiction">Fiction</p>
+      <p className="history">History</p>
+      <p className="nonfiction">Nonfiction</p>
+      <p className="poetry">Poetry</p>
+      <p className="romance">Romance</p>
+      <p className="selfHelp">Self-Help</p>
+    </nav>
   )
 }
 
