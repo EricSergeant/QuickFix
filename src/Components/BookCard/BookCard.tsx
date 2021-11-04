03ce7737632cb29fc52ffb80351c9cab1a988f58
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-11-03 19:03:30 -0500

    Update styling on book card backs

--- src/Components/BookCard/BookCard.tsx
+++ src/Components/BookCard/BookCard.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './BookCard.css';
-import { getSingleBook } from '../../apiCalls'
+// import { getSingleBook } from '../../apiCalls'
+
 interface Props {
   title: string, cover_id: number, key: any, id: string, authors: any
   oneBook: Function
 }
 
 const BookCard: React.FC<Props> = ({ title, cover_id, id, authors, oneBook }) => {
-
-  // const individualBookInfo = (id: string) => {
-  //   getIndividualBook(id)
-  //   oneBook() 
-  // }
-
-
 
   return (
     <div className="book-card-grid">
