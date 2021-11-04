ae127d4e298c34d994ae9322c8dec7383c2c20e1
Raquel Hill <raquelturing@gmail.com>
2021-11-04 12:04:41 -0600

    display dummy book card

--- src/Components/BookCard/BookCard.tsx
+++ src/Components/BookCard/BookCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BookCard.css';
+import { Link } from 'react-router-dom';
 // import { getSingleBook } from '../../apiCalls'
 
 interface Props {
@@ -10,9 +11,10 @@ interface Props {
 const BookCard: React.FC<Props> = ({ title, cover_id, id, authors, oneBook }) => {
 
   return (
+    <Link to={`bookDetails`}>
     <div className="book-card-grid">
       <div className="book-card">
-        <button onClick={() => oneBook(id)}>Book Info</button>
+        {/* <button onClick={() => oneBook(id)}>Book Info</button> */}
         <div className="book-card-inner">
           <div className="book-card-front">
             <img
@@ -29,6 +31,7 @@ const BookCard: React.FC<Props> = ({ title, cover_id, id, authors, oneBook }) =>
         </div>
       </div>
     </div>
+    </Link>
   )
 }
 
