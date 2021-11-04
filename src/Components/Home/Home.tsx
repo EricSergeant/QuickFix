c4de94c86e6eae3187d10b3ccfe6d7cb154959ab
Renee Swick-Ziller <reneeswick@gmail.com>
2021-11-03 18:10:30 -0600

    Make all buttons links to the book card display

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface HomeProps {
   retrieveBooks: Function;
@@ -15,14 +16,16 @@ const Home: React.FC<HomeProps> = ({ retrieveBooks }) => {
       <p>Our goal is to help you select your next best read.</p>
       <p>What genre can we help you find today?</p>
       <div className='genre-button-container'>
-        <button id='biography' onClick= {(event:any) => goToBookPage(event.target.id)}>Biography</button>
-        <button id= 'children' onClick= {() => {retrieveBooks('children')}}>Children</button>
-        <button id='fiction' onClick= {() => {retrieveBooks('fiction')}}>Fiction</button>
-        <button id='history' onClick= {() => {retrieveBooks('history')}}>History</button>
-        <button id='nonfiction' onClick= {() => {retrieveBooks('nonfiction')}}>Nonfiction</button>
-        <button id='poetry' onClick= {() => {retrieveBooks('poetry')}}>Poetry</button>
-        <button id='romance' onClick= {() => {retrieveBooks('romance')}}>Romance</button>
-        <button id='self-help' onClick= {() => {retrieveBooks('self-help')}}>Self-Help</button>
+        <Link to='/books'>
+          <button id='biography' onClick= {(event:any) => goToBookPage(event.target.id)}>Biography</button>
+          <button id= 'children' onClick= {(event:any) => goToBookPage(event.target.id)}>Children</button>
+          <button id='fiction' onClick= {(event:any) => goToBookPage(event.target.id)}>Fiction</button>
+          <button id='history' onClick= {(event:any) => goToBookPage(event.target.id)}>History</button>
+          <button id='nonfiction' onClick= {(event:any) => goToBookPage(event.target.id)}>Nonfiction</button>
+          <button id='poetry' onClick= {(event:any) => goToBookPage(event.target.id)}>Poetry</button>
+          <button id='romance' onClick= {(event:any) => goToBookPage(event.target.id)}>Romance</button>
+          <button id='self-help' onClick= {(event:any) => goToBookPage(event.target.id)}>Self-Help</button>
+        </Link>
       </div>
     </section>
   )
